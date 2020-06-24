093a77a72c15ec1bcc2d935ea7dca8707a7384cf
Dominik Lander <lander45@hotmail.co.uk>
2020-06-24 09:50:12 +0100

    Move api error logic up to page components

--- src/containers/RecipesContainer/RecipesContainer.tsx
+++ src/containers/RecipesContainer/RecipesContainer.tsx
@@ -2,7 +2,6 @@ import { FunctionComponent } from "react";
 import { useRouter, NextRouter } from "next/router";
 import { Typography } from "@material-ui/core";
 
-import ApiError from "../../components/ApiError";
 import Breadcrumbs from "../../components/Breadcrumbs";
 import Pagination from "../../components/Pagination";
 import RecipeList from "../../components/RecipeList";
@@ -15,12 +14,14 @@ interface Props {
   readonly recipes: RecipeFromApi;
   readonly ingredients: string;
   readonly numRecipesPerPage: number;
+  readonly numResultsAllowedByApi: number;
 }
 
 const RecipesContainer: FunctionComponent<Props> = ({
   recipes,
   ingredients,
   numRecipesPerPage,
+  numResultsAllowedByApi
 }) => {
   const router: NextRouter = useRouter();
   const handlePaginationClick = (value: number): Promise<void> =>
@@ -28,44 +29,27 @@ const RecipesContainer: FunctionComponent<Props> = ({
       .push(`/recipes?ingredients=${ingredients}&page=${value}`)
       .then(() => window.scrollTo(0, 0));
 
-  if (!process.env.NEXT_PUBLIC_NUM_RESULTS_ALLOWED_BY_API) {
-    return <ApiError />
-  }
-  let numResultsAllowedByApi: number;
-  try {
-    numResultsAllowedByApi = parseInt(process.env.NEXT_PUBLIC_NUM_RESULTS_ALLOWED_BY_API)
-  }
-  catch {
-    return <ApiError />
-  }
-
   const numRecipes: number = Math.min(recipes.count, numResultsAllowedByApi)
   const numPages: number = Math.ceil(numRecipes / numRecipesPerPage);
 
   return (
-    <>
-      {!recipes?.hits || recipes.status === "error" ? (
-        <ApiError />
-      ) : (
-          <div className={styles.recipesContainer}>
-            <Breadcrumbs
-              crumbs={[
-                { text: "Home", url: "/" },
-                { text: "Recipes" }
-              ]}
-            />
-            <Typography className={styles.pageHeading} variant="h4">
-              {`Recipes featuring ${utils.formatWordsIntoReadableList(ingredients, ",")}`}
-            </Typography>
-            <div className={styles.recipesGrid}>
-              <RecipeList recipes={recipes} />
-            </div>
-            <div className={styles.pagination}>
-              <Pagination count={numPages} handleClick={handlePaginationClick} />
-            </div>
-          </div>
-        )}
-    </>
+    <div className={styles.recipesContainer}>
+      <Breadcrumbs
+        crumbs={[
+          { text: "Home", url: "/" },
+          { text: "Recipes" }
+        ]}
+      />
+      <Typography className={styles.pageHeading} variant="h4">
+        {`Recipes featuring ${utils.formatWordsIntoReadableList(ingredients, ",")}`}
+      </Typography>
+      <div className={styles.recipesGrid}>
+        <RecipeList recipes={recipes} />
+      </div>
+      <div className={styles.pagination}>
+        <Pagination count={numPages} handleClick={handlePaginationClick} />
+      </div>
+    </div>
   );
 };
 
